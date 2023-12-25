9290691dc7972e3dd8ca09aaca9af515157bbe93
Pipexlul <felipgg@gmail.com>
2023-12-24 23:44:40 -0300

    chore: add configuration files and dependencies for linting and formatting

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Options}
+ */
+const config = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "consistent",
+};
+
+export default config;
