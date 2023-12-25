79f2f95a11049b233c78b5466d64403569c38f22
Pipexlul <felipgg@gmail.com>
2023-12-25 17:57:03 -0300

    chore: install ng-zorro library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { en_US, provideNzI18n } from "ng-zorro-antd/i18n";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideNzI18n(en_US),
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+  ],
 };
