75b5a23f211cd4fe9e96e95c48edf0280eee0c85
Pipexlul <felipgg@gmail.com>
2023-12-25 14:11:20 -0300

    chore: install @angular/material dependency

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home-page',
+  selector: "app-home-page",
   standalone: true,
   imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  templateUrl: "./home-page.component.html",
+  styleUrl: "./home-page.component.css",
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
