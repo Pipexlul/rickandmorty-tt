e0877243551aa19836be5d4bf6504403ce586827
Pipexlul <felipgg@gmail.com>
2023-12-26 07:56:17 -0300

    feat: add route for details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from "@angular/router";
 
 import { HomePageComponent } from "./views/home-page/home-page.component";
+import { DetailsPageComponent } from "./views/details-page/details-page.component";
 
 export const routes: Routes = [
   {
     path: "",
     component: HomePageComponent,
+  },
+  {
+    path: "details/:id",
+    component: DetailsPageComponent,
   },
 ];
