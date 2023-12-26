ae05d3ae5e7207d8684f7482097d7ce2b4eb4ca4
Pipexlul <felipgg@gmail.com>
2023-12-26 03:41:28 -0300

    chore: add card list component with dummy data to home page

--- src/app/views/home-page/home-page.component.ts
+++ src/app/views/home-page/home-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from "@angular/core";
 
+import { CardListComponent } from "../../components/card-list/card-list.component";
+import { CardDTO } from "../../interfaces/card.interface";
+
 @Component({
   selector: "app-home-page",
   standalone: true,
-  imports: [],
+  imports: [CardListComponent],
   templateUrl: "./home-page.component.html",
   styleUrl: "./home-page.component.css",
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  testArray: CardDTO[] = Array.from({ length: 100 }, (_, i) => {
+    return {
+      id: i,
+      name: `tEst lol ${i}`,
+      gender: Math.random() <= 0.5 ? "Male" : "Female",
+      image: "",
+      status: Math.random() <= 0.5 ? "Alive" : "Dead",
+      species: Math.random() <= 0.5 ? "Human" : "Alien",
+    };
+  });
+}
